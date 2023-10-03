bd4fe3ff714c1cdfbe9ae57afee8ef7514ac838d
chendeng <530972294@qq.com>
2023-10-03 23:50:09 +0800

    ci: 集成husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     'plugin:vue/vue3-essential',
-
     'standard-with-typescript'
-
   ],
   parser: 'vue-eslint-parser',
   overrides: [
