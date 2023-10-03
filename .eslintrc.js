c0c1cc0cc8fa51a8b39031e3828bd6c58313941f
chendeng <530972294@qq.com>
2023-10-03 22:45:01 +0800

    build: 初始化vite-vue-ts-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
+
+    'standard-with-typescript'
+
+  ],
+  parser: 'vue-eslint-parser',
+  overrides: [
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: [
+      'tsconfig.json'
+    ],
+    extraFileExtensions: ['.vue']
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Vue</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
