6199fc014ddc04260a4172ce89be644e16af6901
chendeng <530972294@qq.com>
2023-10-03 22:48:53 +0800

    build: 增加less支持

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>Vue</div>
+  <div class="container">
+    Vue
+  </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.container {
+  width: 100%;
+  height: 100%
+}
 </style>
